88ef38dcbbb25e618b3c846fa583580f6725d293
Sultan Mahmud <smrsaimon960@gmail.com>
2021-04-01 14:08:40 +0600

    djhsljh

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { userContext } from "../../App";
 import "./CheckOut.css";
 
 const CheckOut = () => {
+  const { user } = useContext(userContext);
   const { id } = useParams();
-
+  const { displayName, email } = user;
   const [buyProduct, setBuyProduct] = useState({});
+  const [checkOut, setCheckOut] = useState(false);
+
+  const { name, price, wight, img } = buyProduct;
 
   useEffect(() => {
     fetch(`http://localhost:5000/buyProduct/${id}`)
@@ -15,34 +20,65 @@ const CheckOut = () => {
       });
   }, [id]);
 
+  console.log(user);
+  const hendelCheckOut = () => {
+    const cheeckOutDetails = { img, name, price, wight, displayName, email, date: new Date() };
+
+    console.log(cheeckOutDetails);
+
+    fetch("http://localhost:5000/checkOut", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cheeckOutDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCheckOut(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div className="conatiner">
-        <div class="divTable blueTable">
-          <div class="divTableHeading">
-            <div class="divTableRow">
-              <div class="divTableHead">Discription</div>
-              <div class="divTableHead">Quintity</div>
-              <div class="divTableHead">Price</div>
+        {checkOut && <span> SuccessFully place Your Order. Go to Order Page to see Your all Order</span>}
+
+        {!checkOut && (
+          <div class="divTable blueTable">
+            <div class="divTableHeading">
+              <div class="divTableRow">
+                <div class="divTableHead">Discription</div>
+                <div class="divTableHead">Quintity</div>
+                <div class="divTableHead">Price</div>
+              </div>
+            </div>
+            <div class="divTableBody">
+              <div class="divTableRow">
+                <div class="divTableCell">{buyProduct.name}</div>
+                <div class="divTableCell">1</div>
+                <div class="divTableCell">${buyProduct.price}</div>
+              </div>
+            </div>
+            <div class="divTableFoot tableFootStyle">
+              <div class="divTableRow">
+                <div class="divTableCell">Total Price:&nbsp;</div>
+                <div class="divTableCell">&nbsp;</div>
+                <div class="divTableCell">${buyProduct.price}</div>
+              </div>
             </div>
           </div>
-          <div class="divTableBody">
-            <div class="divTableRow">
-              <div class="divTableCell">{buyProduct.name}</div>
-              <div class="divTableCell">1</div>
-              <div class="divTableCell">${buyProduct.price}</div>
-            </div>
-          </div>
-          <div class="divTableFoot tableFootStyle">
-            <div class="divTableRow">
-              <div class="divTableCell">Total Price:&nbsp;</div>
-              <div class="divTableCell">&nbsp;</div>
-              <div class="divTableCell">${buyProduct.price}</div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
-      <button className="btncheckOut">Check Out</button>
+
+      {!checkOut && (
+        <button onClick={hendelCheckOut} className="btncheckOut">
+          Check Out
+        </button>
+      )}
     </>
   );
 };
